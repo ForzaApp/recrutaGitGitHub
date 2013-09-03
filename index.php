afd4e749a63b669a2bf208c1d74ab116a7c523f0
Alexsandro Andre <contato@alexsandroandre.com.br>
2013-09-03 00:34:03 -0300

    adicionado comentrio

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/vendor/autoload.php'; 
 
 $app = new Silex\Application(); 
-
+//mostra o hello name 
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
